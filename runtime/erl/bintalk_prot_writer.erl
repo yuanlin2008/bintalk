c0a481b5eaed674db66c2e2df208dfb04733e4bb
yuanlin <yuanlin2008@hotmail.com>
2013-03-19 18:10:30 +0800

    erlang runtime

--- runtime/erl/bintalk_prot_writer.erl
+++ runtime/erl/bintalk_prot_writer.erl
@@ -1,75 +1,41 @@
+%% @doc bintalk binary protocol writer.
+%%
 -module(bintalk_prot_writer).
 
-%%----------------------------------------------------------------------
-%% Exported Functions
-%%----------------------------------------------------------------------
--export([
-	write_mid/1,
-	write/3
-	]).
+-export([write_mid/1]).
+-export([write/3]).
 
-%%-----------------------------------------------------------------
-%% Func: write_mid/1
-%% Returns: 
-%%		 Binary
-%%-----------------------------------------------------------------
-write_mid(Mid)->
-	<<Mid:16/little-unsigned-integer>>.
-%%-----------------------------------------------------------------
-%% Func: write/3
-%% Args: 
-%%       Type - data type.
-%%		 Array - is array.
-%%       Value - data type value.
-%% Returns: 
-%%		 Binary
-%%-----------------------------------------------------------------
-%% array.
+%% @doc Write a message id.
+-spec write_mid(Mid::integer()) -> binary().
+write_mid(Mid)-> <<Mid:16/little-unsigned-integer>>.
+
+
+%% @doc Write the value of a specific type.
+-spec write(T::atom(), IsArr::boolean(), V::any()) -> binary().
 write(T, true, V) ->
-	DSize = write_dyn_size(length(V)),
+	Len = write_dyn_size(length(V)),
 	Array = [write(T, false, I) || I <- V],
-	iolist_to_binary([DSize|Array]);
-%% normal types.
-write(int64, false, V) ->
-	<<V:64/little-signed-integer>>;
-write(uint64, false, V) ->
-	<<V:64/little-unsigned-integer>>;
-write(double, false, V) ->
-	<<V:64/little-float>>;
-write(float, false, V) ->
-	<<V:32/little-float>>;
-write(int32, false, V) ->
-	<<V:32/little-signed-integer>>;
-write(uint32, false, V) ->
-	<<V:32/little-unsigned-integer>>;
-write(int16, false, V) ->
-	<<V:16/little-signed-integer>>;
-write(uint16, false, V) ->
-	<<V:16/little-unsigned-integer>>;
-write(int8, false, V) ->
-	<<V:8/little-signed-integer>>;
-write(uint8, false, V) ->
-	<<V:8/little-unsigned-integer>>;
-write(bool, false, V) ->
-	case V of
-		false -> <<0:8>>;
-		true -> <<1:8>>
-	end;
-write(string, false, V) ->
-	iolist_to_binary([write_dyn_size(length(V)),V]);
-write(binary, true, V) ->
-	iolist_to_binary([write_dyn_size(length(V)),V]);
-%% enum
-write(enum, false, V) ->
-	write(uint8, false, V);
-%% user type.
-write(UT, false, V) ->
-	UT:serialize(V).
+	iolist_to_binary([Len|Array]);
+write(int64, false, V) -> <<V:64/little-signed-integer>>;
+write(uint64, false, V) -> <<V:64/little-unsigned-integer>>;
+write(double, false, V) -> <<V:64/little-float>>;
+write(float, false, V) -> <<V:32/little-float>>;
+write(int32, false, V) -> <<V:32/little-signed-integer>>;
+write(uint32, false, V) -> <<V:32/little-unsigned-integer>>;
+write(int16, false, V) -> <<V:16/little-signed-integer>>;
+write(uint16, false, V) -> <<V:16/little-unsigned-integer>>;
+write(int8, false, V) -> <<V:8/little-signed-integer>>;
+write(uint8, false, V) -> <<V:8/little-unsigned-integer>>;
+write(bool, false, true) -> <<1:8>>;
+write(bool, false, false) -> <<0:8>>;
+write(string, false, V) -> iolist_to_binary([write_dyn_size(length(V)),V]);
+write(binary, true, V) -> iolist_to_binary([write_dyn_size(length(V)),V]);
+write(enum, false, V) -> write(uint8, false, V);
+write(UT, false, V) -> UT:serialize(V).
 
 %%----------------------------------------------------------------------
 %% Local Functions
 %%----------------------------------------------------------------------
-%% dynamic size.
 write_dyn_size(S) when is_integer(S) andalso S >= 0 ->
 	if
 		S =< 16#3F ->
